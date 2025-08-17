31422afcc341ba014ebb2daf85a9e19a335547a1
akash <166794369+akasha17@users.noreply.github.com>
2025-08-17 23:14:26 +0530

    33697

--- home/views.py
+++ home/views.py
@@ -12,3 +12,6 @@ def homepage(request):
 
 def about_us(request):
     return render(request, 'about.html')
+
+def custom_404_view(request, exception):
+    return render(request, '404.html',status=404)
